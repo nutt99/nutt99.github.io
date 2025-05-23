e06e88d9e0fb23a002eb771ecb45f99f58cb78ee
nutt99 <NataGaming766@gmail.com>
2025-05-24 02:44:18 +0700

    update splash animasi

--- portofolio/assets/scripts/animation_handler.js
+++ portofolio/assets/scripts/animation_handler.js
@@ -2,6 +2,10 @@ const heroSec = document.getElementById("heroId");
 const projectInfo = document.getElementById("project-info");
 const progressBar = document.getElementById("progress-bar");
 const imagePrev = document.getElementById("image-prev")
+const title1 = document.getElementById("title1")
+const title2 = document.getElementById("title2")
+const splash = document.getElementById("splash")
+const realcontent = document.getElementById("realcontent")
 
 var pInfoHeading = document.getElementById("p-info-heading")
 var pInfoDetail = document.getElementById("p-info-detail")
@@ -53,6 +57,24 @@ projectInfo.addEventListener("animationend", function (e) {
     projectInfo.classList.remove("slide-down");
   }
 });
+
+title1.addEventListener("animationend", function (e) {
+  if(e.animationName === "slideInLR"){
+    title1.classList.remove("title1")
+    title1.classList.add("endtitle1")
+  }
+})
+
+title2.addEventListener("animationend", function (e) {
+  if (e.animationName === "slideInRL") {
+    title2.classList.remove("title2")
+    title2.classList.add("endtitle2")
+  }
+  else if(e.animationName === "endslideInRL"){
+    splash.classList.add("d-none")
+    realcontent.classList.remove("d-none")
+  }
+})
 
 progressBar.addEventListener("animationend", function(){
     indexData += 1;
